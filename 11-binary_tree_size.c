21734a482f1c7caf211afadf6a2ccd4c170a26af
Samuel Amihere <amisam2000@gmail.com>
2023-11-03 19:50:32 +0800

    Size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -10,7 +10,15 @@
  */
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-	if (tree == NULL)
-		return (0);
+	size_t s;
+
+	s = 0;
+	if (tree)
+	{
+		s += 1;
+		s += binary_tree_size(tree->left);
+		s += binary_tree_size(tree->right);
+	}
+	return (s);
 }
 
