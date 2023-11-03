26a2f97be247e184c519e43ff1699a4763f75d0b
Samuel Amihere <amisam2000@gmail.com>
2023-11-03 19:32:29 +0800

    Depth

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,8 +9,8 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	if (tree || tree->parent)
-		return (1 + binary_tree_depth(tree->parent));
-	return (0);
+	if (!tree || !(tree->parent))
+		return (0);
+	return (1 + binary_tree_depth(tree->parent));
 }
 
