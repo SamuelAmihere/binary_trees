de57102372b7d1380c0059ef63af8ebbe0d21173
Samuel Amihere <amisam2000@gmail.com>
2023-11-03 20:12:24 +0800

    Leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -13,7 +13,8 @@ size_t binary_tree_leaves(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		if (!tree->left && !tree->right)
+			leaves += 1;
 		leaves += binary_tree_leaves(tree->left);
 		leaves += binary_tree_leaves(tree->right);
 	}
