a705f585471255fa185f94bdc61948b1c8cb5744
Samuel Amihere <amisam2000@gmail.com>
2023-11-02 23:25:54 -0700

    Height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -10,6 +10,17 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
+	size_t lt, rt;
+
 	if (tree == NUL)
 		return (0);
+
+	lt = 0;
+	rt = 0;
+
+	lt = tree->left ? 1 + binary_tree_height(tree->left) : 0;
+	rt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
+
+	return (((rt < lt) ? lt : rt));
+
 }
